775451cc1c5c768108af95c6b65033f982e8e6e8
bzamanbd <riponzamanbd@gmail.com>
2024-06-23 11:26:37 +0600

    Added: express.json middleware in index

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express" 
 import 'dotenv/config' 
+import dbDisconnect from './middlewares/db_disconnect.js'
 
 const app = express() 
 const port = process.env.PORT || 3005 
+
+app.use(express.json())
+
+
 
 app.listen(port,()=>console.log(`server runs on http://localhost:${port}`))
