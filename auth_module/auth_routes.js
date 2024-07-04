2724e9707a29f2c33f67b17ae71001762248b220
bzamanbd <riponzamanbd@gmail.com>
2024-07-05 00:46:00 +0600

    ADDED:Custom_middleware_for_avatar_image_compressor_and_uploader

--- auth_module/auth_routes.js
+++ auth_module/auth_routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";  
 import { signup, signin} from "./auth_controllers.js"; 
-import { upload } from "../middlewares/multer_middleware.js";
+import { upload,compressImage } from "../middlewares/image_compressor.js";
 
 const routes = Router() 
 
-routes.post("/signup", upload.single('avatar'), signup)
+routes.post("/signup", upload.single('avatar'), compressImage('avatar', { width: 50, height: 50, quality: 80 }),signup)
  
 routes.post("/signin", signin)
 
